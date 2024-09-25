aea50e26ee0e0fbb0314c5f1deac59c0da4e1ef4
Abhishek Kumar <abhikumar26122001@gmail.com>
2024-09-25 20:31:01 +0530

    created post,put,get and delete api

--- controller/todoController.js
+++ controller/todoController.js
@@ -16,6 +16,50 @@ const addTodo = async(req,res)=>{
     }
 }
 
+const getTodo = async(req,res)=>{
+    try{
+        const todoList = await todoModel.find({},{_id:1,title:1,description:1});
+        res.status(201).json({status:'201',message:'Data saved.',data:todoList})
+        
+    }catch(err){
+        res.status(400).json({status:400,message:'Something went wrong.'})
+    }
+}
+
+const updateTodo = async(req,res)=>{
+    try{
+        const {title,description,id} = req.body;
+        const findTodo = await todoModel.findOne({_id:id});
+        if(!findTodo) return res.status(400).json({status:400,error:"Provide correct details."});
+
+        const updatedData = await todoModel.updateOne({_id:id},{title,description});
+        const updated = await todoModel.findOne({_id:id},{title:1,_id:1,description:1});
+
+        res.status(201).json({status:'201',message:'Data updated successfully.',data:updated})
+    }catch(err){
+        console.log(err,"error")
+        res.status(400).json({status:400,message:'Something went wrong.'})
+    }
+}
+
+const deleteTodo = async(req,res)=>{
+    try{
+        const {title,description,id} = req.body;
+        const findTodo = await todoModel.findOne({_id:id});
+        if(!findTodo) return res.status(400).json({status:400,error:"Provide correct details."});
+
+        const updatedData = await todoModel.deleteOne({_id:id})
+
+        res.status(201).json({status:'201',message:'Data deleted successfully.',data:updatedData})
+
+    }catch(err){
+        res.status(400).json({status:400,message:'Something went wrong.'})
+    }
+}
+
 module.exports={
-    addTodo:addTodo
+    addTodo:addTodo,
+    todoList:getTodo,
+    updateTodo:updateTodo,
+    deleteTodo:deleteTodo
 }
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const Router = express.Router();
-const {addTodo} = require('../controller/todoController');
+const {addTodo,todoList,updateTodo,deleteTodo} = require('../controller/todoController');
 
 Router.post('/add',addTodo);
+Router.get('/get',todoList);
+Router.put('/put',updateTodo);
+Router.delete('/delete',deleteTodo);
+
 
 module.exports=Router;
